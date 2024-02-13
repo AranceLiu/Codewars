54bbbbef349d77ab19742b6c0ab2a68111f7e838
AranceLiu <fish70309@gmail.com>
2024-02-13 13:13:15 +0800

    Reduce: use Digital root for reduce the value in js

---
+++ 04_digital_root.js
@@ -0,0 +1,9 @@
+function digitalRoot(n) {
+  return n == 0 ? 0 : 1 + ((n - 1) % 9);
+}
+
+console.log(digitalRoot(0)); // 0
+console.log(digitalRoot(16)); // 7
+console.log(digitalRoot(942)); // 6
+console.log(digitalRoot(132189)); //6
+console.log(digitalRoot(493193)); //2

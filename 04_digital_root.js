3cd01c4e7d4412777ef5b2a4ab367e582729c774
AranceLiu <fish70309@gmail.com>
2024-02-15 15:57:02 +0800

    Add: find the digits root of birthday

--- 04_digital_root.js
+++ 04_digital_root.js
@@ -7,3 +7,4 @@ console.log(digitalRoot(16)); // 7
 console.log(digitalRoot(942)); // 6
 console.log(digitalRoot(132189)); //6
 console.log(digitalRoot(493193)); //2
+console.log(digitalRoot(19930221)); //9
